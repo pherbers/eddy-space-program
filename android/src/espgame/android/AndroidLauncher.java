7ef766cea2924dff0d637bbb5485157d08bd4876
Patrick Herbers <patrick.herbers@rub.de>
2015-08-24 16:16:16 +0200

    Created initial libGDX files.

---
+++ android/src/espgame/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package espgame.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import espgame.ESPGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new ESPGame(), config);
+	}
+}
